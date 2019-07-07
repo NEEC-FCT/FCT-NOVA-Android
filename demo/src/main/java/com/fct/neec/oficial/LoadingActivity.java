95e7f53b6902d79ef08f60b76dc6affe41a7cd50
veloso14 <jm.veloso@campus.fct.unl.pt>
2019-07-07 14:30:07 +0100

    Loading Screen feito

---
+++ demo/src/main/java/com/fct/neec/oficial/LoadingActivity.java
@@ -0,0 +1,41 @@
+package com.fct.neec.oficial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+
+
+    private static  int SPLASH_TIME_OUT = 2100;
+
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_loading_actibity);
+
+
+
+        new Handler().postDelayed(new  Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(LoadingActivity.this,DemoActivity.class);
+                startActivity(homeIntent);
+                this.finish();
+            }
+            protected  void finish(){
+
+            }
+        },SPLASH_TIME_OUT);
+
+
+
+
+
+    }
+}
