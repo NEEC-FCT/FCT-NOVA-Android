f0a4a9c591a1673af9079152c81126b2073931bc
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-09-23 11:58:12 +0100

    AsyncTask  com loading Dialog

--- demo/src/main/java/com/fct/neec/oficial/PerdidosMasAchados/ImageFilePath.java
+++ demo/src/main/java/com/fct/neec/oficial/PerdidosMasAchados/ImageFilePath.java
@@ -11,8 +11,6 @@ import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 
-//import android.provider.<span id="IL_AD11" class="IL_AD">MediaStore</span>;
-
 @SuppressLint("NewApi")
 @TargetApi(Build.VERSION_CODES.KITKAT)
 public class ImageFilePath {
--- demo/src/main/java/com/fct/neec/oficial/PerdidosMasAchados/PerdidosEAchados.java
+++ demo/src/main/java/com/fct/neec/oficial/PerdidosMasAchados/PerdidosEAchados.java
@@ -1,6 +1,7 @@
 package com.fct.neec.oficial.PerdidosMasAchados;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
@@ -35,15 +36,11 @@ import java.util.Map;
 public class PerdidosEAchados extends AppCompatActivity {
 
     Dialog myDialog;
+    static ProgressDialog dialog;
     public static final int PICK_IMAGE = 1;
 
 
 
-
-    int serverResponseCode = 0;
-
-
-    String upLoadServerUri = "https://fcthub.neec-fct.com/PHP/uploadPerdidos.php";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,6 +64,11 @@ public class PerdidosEAchados extends AppCompatActivity {
     }
 
 
+    public static void uploadDone(){
+        Log.d("Uploader" , "Upload done");
+        dialog.dismiss();
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -78,7 +80,9 @@ public class PerdidosEAchados extends AppCompatActivity {
 
             Log.i("PERDIDOS", "onActivityResult: file path : " + realPath);
             //setup params
-            uploadFile(realPath);
+             dialog = ProgressDialog.show(PerdidosEAchados.this, "",
+                    "Enviando,por favor aguarde ... ", true);
+            new UploadBackground().execute(realPath,"nome","local","descricao","categoria");
 
 
         } else {
@@ -124,150 +128,7 @@ public class PerdidosEAchados extends AppCompatActivity {
     }
 
 
-    public int uploadFile(String sourceFileUri) {
 
-        String fileName = sourceFileUri;
-
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        String lineEnd = "\r\n";
-        String twoHyphens = "--";
-        String boundary = "*****";
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
-        File sourceFile = new File(sourceFileUri);
-
-        if (!sourceFile.isFile()) {
-
-            Log.e("uploadFile", "Source File not exist :" );
-            return 0;
-        }
-        else
-        {
-            try {
-
-                // open a URL connection to the Servlet
-                FileInputStream fileInputStream = new FileInputStream(sourceFile);
-                URL url = new URL(upLoadServerUri);
-
-                // Open a HTTP  connection to  the URL
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setDoInput(true); // Allow Inputs
-                conn.setDoOutput(true); // Allow Outputs
-                conn.setUseCaches(false); // Don't use a Cached Copy
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Connection", "Keep-Alive");
-                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-                conn.setRequestProperty("fileToUpload", fileName);
-
-                dos = new DataOutputStream(conn.getOutputStream());
-
-                dos.writeBytes(twoHyphens + boundary + lineEnd);
-                dos.writeBytes("Content-Disposition: form-data; name=\"fileToUpload\";filename="+ fileName + "" + lineEnd);
-                dos.writeBytes(lineEnd);
-
-                // create a buffer of  maximum size
-                bytesAvailable = fileInputStream.available();
-
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                buffer = new byte[bufferSize];
-
-                // read file and write it into form...
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                while (bytesRead > 0) {
-
-                    dos.write(buffer, 0, bufferSize);
-                    bytesAvailable = fileInputStream.available();
-                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                }
-
-                // send multipart form data necesssary after file data...
-                dos.writeBytes(lineEnd);
-                dos.writeBytes(twoHyphens + boundary  + lineEnd);
-
-                //Send parameters
-                //Nome
-                dos.writeBytes("Content-Disposition: form-data; name=\"nome\";" + lineEnd);
-                dos.writeBytes(lineEnd);
-                dos.writeBytes("Veloso" + lineEnd);
-                //local
-                dos.writeBytes(twoHyphens + boundary  + lineEnd);
-                dos.writeBytes("Content-Disposition: form-data; name=\"local\";" + lineEnd);
-                dos.writeBytes(lineEnd);
-                dos.writeBytes("local" + lineEnd);
-                //descricao
-                dos.writeBytes(twoHyphens + boundary  + lineEnd);
-                dos.writeBytes("Content-Disposition: form-data; name=\"descricao\";" + lineEnd);
-                dos.writeBytes(lineEnd);
-                dos.writeBytes("descricao" + lineEnd);
-                dos.writeBytes(twoHyphens + boundary  + lineEnd);
-                dos.writeBytes("Content-Disposition: form-data; name=\"categoria\";" + lineEnd);
-                dos.writeBytes(lineEnd);
-                dos.writeBytes("categoria" + lineEnd);
-                //Fim do Pedido
-                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-                // Responses from the server (code and message)
-                serverResponseCode = conn.getResponseCode();
-                String serverResponseMessage = conn.getResponseMessage();
-
-                Log.i("uploadFile", "HTTP Response is : "+ serverResponseMessage + ": " + serverResponseCode);
-
-                if(serverResponseCode == 200){
-
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-
-                            Toast.makeText(PerdidosEAchados.this, "File Upload Complete.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-
-                //close the streams //
-                fileInputStream.close();
-                dos.flush();
-                dos.close();
-
-            } catch (MalformedURLException ex) {
-
-
-                ex.printStackTrace();
-
-                runOnUiThread(new Runnable() {
-                    public void run() {
-
-                        Toast.makeText(PerdidosEAchados.this, "MalformedURLException",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
-            } catch (Exception e) {
-
-
-                e.printStackTrace();
-
-                runOnUiThread(new Runnable() {
-                    public void run() {
-
-                        Toast.makeText(PerdidosEAchados.this, "Got Exception : see logcat ",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-                Log.d("Upload file to s", "Exception : "
-                        + e.getMessage(), e);
-            }
-
-            return serverResponseCode;
-
-        } // End else block
-    }
 
 
 }
